acfdde35523963bec866d6ec19d956d3383b30dd
wmtumanday <wiljohnyumanday@gmail.com>
2024-08-29 19:01:16 +0800

    Initialized maven project

---
+++ src/main/java/com/wmt/smartparking/SmartParkingApplication.java
@@ -0,0 +1,16 @@
+package com.wmt.smartparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wmtumanday
+ */
+@SpringBootApplication
+public class SmartParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wmt/smartparking/SmartParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmt.smartparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
